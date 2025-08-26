c35e85ef60d90a47c4d9f9a32e195c6a4e22bb44
maximilian-invest <maximilian@hoelzl.investments>
2025-08-26 14:39:23 +0200

    feat: update pricebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { PriceBar } from "@/components/price-bar";
 import React, { useEffect, useMemo, useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
